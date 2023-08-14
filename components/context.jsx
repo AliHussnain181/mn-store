d88b83c38636d204b6cec10a5b75bf3df9c254e0
ali <ali12greena@gmail.com>
2023-08-14 03:54:51 -0700

    first commit

--- components/context.jsx
+++ components/context.jsx
@@ -7,7 +7,7 @@ export const ContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/auth/me")
+    fetch("/api/auth/me")
       .then((res) => res.json())
       .then((data) => {
         if (data.success) setUser(data.user);
